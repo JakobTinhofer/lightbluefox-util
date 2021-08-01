21b67cc528e21d51428e8dc7f9581a0816e26ba9
JakobTinhofer <65621944+JakobTinhofer@users.noreply.github.com>
2021-08-01 18:47:02 +0200

    * Reformated Color * Added equality operators to color * Added very basic testing project * Added color tests to testing project * Fixed multiple bugs in the Color hex string parsing

--- Util/Types/Color.cs
+++ Util/Types/Color.cs
@@ -7,13 +7,16 @@ namespace LightBlueFox.Util.Types
 {
     public struct Color
     {
+        #region Private Members
         private byte r;
         private byte g;
         private byte b;
         private byte a;
         private string hexRGB;
         private string hexRGBA;
+        #endregion
 
+        #region Hex String Parsing
         private void UpdateStrings()
         {
             hexRGB = "#" + r.ToString("X2") + g.ToString("X2") + b.ToString("X2"); hexRGBA = HexRGB + a.ToString("X2");
@@ -22,16 +25,16 @@ namespace LightBlueFox.Util.Types
         {
             value = value.Trim().ToLower();
             if (value.StartsWith("#"))
-                value = value.Substring(0, 1);
+                value = value.Substring(1, value.Length - 1);
             else if (value.StartsWith("0x"))
-                value = value.Substring(0, 2);
+                value = value.Substring(2, value.Length - 2);
             if (Regex.IsMatch(value, "(?i)[abcdef0123456789]{6}([abcdef0123456789]{2})?"))
             {
-                r = Convert.ToByte(value.Substring(0, 2));
-                g = Convert.ToByte(value.Substring(2, 2));
-                b = Convert.ToByte(value.Substring(4, 2));
+                r = byte.Parse(value.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+                g = byte.Parse(value.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+                b = byte.Parse(value.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
                 if (value.Length == 8)
-                    a = Convert.ToByte(value.Substring(6, 2));
+                    a = byte.Parse(value.Substring(6, 2), System.Globalization.NumberStyles.HexNumber);
                 UpdateStrings();
             }
             else
@@ -39,7 +42,55 @@ namespace LightBlueFox.Util.Types
                 throw new ArgumentException("Invalid format for hex string! Expected: 0xFF00AA or FF00AA or FF00AA44 or 0xFF00AA44 or #FF00AA or #FF00AA44!");
             }
         }
+        #endregion
 
+        #region Operator Overrrides
+        /// <summary>
+        /// Equality operator.
+        /// </summary>
+        public static bool operator ==(Color c1, Color c2)
+        {
+            if (c1.R == c2.R && c1.G == c2.G && c1.B == c2.B && c1.Alpha == c2.Alpha) return true;
+            return false;
+        }
+        /// <summary>
+        /// Inequality operator.
+        /// </summary>
+        public static bool operator !=(Color c1, Color c2)
+        {
+            return !(c1 == c2);
+        }
+
+        /// <summary>
+        /// Auto-generated Equals method.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            return obj is Color color &&
+                   r == color.r &&
+                   g == color.g &&
+                   b == color.b &&
+                   a == color.a;
+        }
+
+        /// <summary>
+        /// Auto-generated GetHashCode method.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            var hashCode = 397963109;
+            hashCode = hashCode * -1521134295 + r.GetHashCode();
+            hashCode = hashCode * -1521134295 + g.GetHashCode();
+            hashCode = hashCode * -1521134295 + b.GetHashCode();
+            hashCode = hashCode * -1521134295 + a.GetHashCode();
+            return hashCode;
+        }
+        #endregion
+
+        #region Public Properties
         /// <summary>
         /// The value of the red channel, from 0 to 255
         /// </summary>
@@ -87,6 +138,9 @@ namespace LightBlueFox.Util.Types
             }
         }
 
+        #endregion
+
+        #region Constructors
         /// <summary>
         /// Creates a new color struct using the values given
         /// </summary>
@@ -113,7 +167,13 @@ namespace LightBlueFox.Util.Types
             hexRGBA = "";
             ParseString(str);
         }
+        #endregion
 
+        #region Converters
+        /// <summary>
+        /// Gets the console color closest to the current color.
+        /// </summary>
+        /// <returns></returns>
         public ConsoleColor ToClosestConsoleColor()
         {
             ConsoleColor ret = 0;
@@ -134,5 +194,7 @@ namespace LightBlueFox.Util.Types
             }
             return ret;
         }
+        #endregion
+
     }
 }
