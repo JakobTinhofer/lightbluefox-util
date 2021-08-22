9958fb7ea726934b77e2e93bb2e3322441389e08
TINHOFER Jakob <jak.tinhofer@billrothgymnasium.at>
2021-08-22 18:06:45 +0200

    added Cooldown implementations

---
+++ Util/Cooldown.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LightBlueFox.Util
+{
+    /// <summary>
+    /// A template for methods that can be triggered through cooldowns.
+    /// </summary>
+    /// <param name="args">Any arguments for that method. Accepts null!</param>
+    public delegate void CooldownAction(object[] args);
+    /// <summary>
+    /// A class that can be used to allow actions to be reused only after a certain interval.
+    /// </summary>
+    public class Cooldown
+    {
+        /// <summary>
+        /// Creates a new Cooldown object. Also assigns the action to <see cref="CooldownActionTriggered"/>.
+        /// </summary>
+        public Cooldown(int timeInMS, CooldownAction action)
+        {
+            DurationInMilliseconds = timeInMS;
+            CooldownActionTriggered += action;
+        }
+
+        /// <summary>
+        /// Creates a new Cooldown object. Note that by default, this will not do anything when triggered. In order to do that, please assign an action to <see cref="CooldownActionTriggered"/>.
+        /// </summary>
+        public Cooldown(int timeInMS)
+        {
+            DurationInMilliseconds = timeInMS;
+        }
+        /// <summary>
+        /// How long the interval between two triggers needs to be.
+        /// </summary>
+        public int DurationInMilliseconds { get; set; }
+        /// <summary>
+        /// Assign all actions that should be triggered here.
+        /// </summary>
+        public event CooldownAction CooldownActionTriggered;
+        /// <summary>
+        /// If this is set to true, all actions are automaticaly triggered once the timer is reset.
+        /// </summary>
+        public bool AutoTrigger { get; set; } = false;
+        /// <summary>
+        /// Whether or not the action is currently available.
+        /// </summary>
+        public bool Ready { get; private set; } = true;
+
+        private AutoResetEvent ev = new AutoResetEvent(true);
+
+        private async void StartReset()
+        {
+            await Task.Delay(DurationInMilliseconds);
+            Ready = true;
+            ev.Set();
+            if (AutoTrigger)
+                TryTrigger(null);
+        }
+
+        /// <summary>
+        /// Tries to trigger the action. If the action is still on cooldown, returns false.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns><see cref="Ready"/> at the moment of the function call</returns>        
+        public bool TryTrigger(object[] args)
+        {
+            if (Ready)
+            {
+                Ready = false;
+                ev.Reset();
+                CooldownActionTriggered.BeginInvoke(args, null, null);
+                StartReset();
+                return true;
+            }    
+            return false;
+        }
+
+        /// <summary>
+        /// This will wait until the cooldown is over, and then trigger the action.
+        /// </summary>
+        /// <param name="args"></param>
+        public void WaitTillTrigger(object[] args)
+        {
+            ev.WaitOne();
+            Ready = false;
+            CooldownActionTriggered.BeginInvoke(args, null, null);
+            StartReset();
+        }
+    
+    }
+}
---
+++ Util/LongCooldown.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LightBlueFox.Util
+{
+    /// <summary>
+    /// A cooldown for longer timespans.
+    /// </summary>
+    public class LongCooldown
+    {
+        /// <summary>
+        /// The Point of time that the action was last triggered.
+        /// </summary>
+        public DateTime StartingPoint { get; private set; }
+        /// <summary>
+        /// The time to wait after use.
+        /// </summary>
+        public TimeSpan Duration { get; private set; }
+        /// <summary>
+        /// Creates a new cooldown object. The <see cref="StartingPoint"/> is set to <see cref="DateTime.Now"/> - <see cref="Duration"/>
+        /// </summary>
+        public LongCooldown(TimeSpan duration)
+        {
+            StartingPoint = DateTime.Now - duration;
+            Duration = duration;
+        }
+        /// <summary>
+        /// Whether or not the cooldown is run down.
+        /// </summary>
+        public bool Check()
+        {
+            if ((StartingPoint + Duration) >= DateTime.Now)
+                return true;
+            return false;
+        }
+        /// <summary>
+        /// First checks whether or not cooldown is available. If so, resets and sets <see cref="StartingPoint"/> to <see cref="DateTime.Now"/> + <see cref="Duration"/>.
+        /// </summary>
+        /// <returns></returns>
+        public bool Trigger()
+        {
+            if (Check())
+            {
+                StartingPoint = DateTime.Now;
+                return true;
+            }
+            return false;
+        }
+    }
+}
