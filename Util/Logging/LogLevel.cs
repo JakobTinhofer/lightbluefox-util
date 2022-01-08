05364a4dd621c4adbdbafa2e16e330b9066cff63
TINHOFER Jakob <jak.tinhofer@billrothgymnasium.at>
2022-01-09 00:49:37 +0100

    Add success loglevel

--- Util/Logging/LogLevel.cs
+++ Util/Logging/LogLevel.cs
@@ -6,8 +6,12 @@ using System.Text;
 
 namespace LightBlueFox.Util.Logging
 {
+    /// <summary>
+    /// Defines in what context the message is to be interpreted. Is this an error? Or just debug info?
+    /// </summary>
     public class LogLevel
     {
+        #region Log Level Members
         public static LogLevel operator |(LogLevel a, LogLevel b)
         {
             return new LogLevel(a, b);
@@ -31,7 +35,6 @@ namespace LightBlueFox.Util.Logging
         }
 
         private List<LogLevel> combinedLogLevels;
-
         /// <summary>
         /// The color that if the LogWriter allows it, is added to the tag of the message.
         /// </summary>
@@ -44,6 +47,9 @@ namespace LightBlueFox.Util.Logging
         /// If this is false, this cannot be used to print a message to the console. This is probably because this is combined loglevel.
         /// </summary>
         public readonly bool Printable;
+        #endregion
+
+        #region Constructors
         private LogLevel(Color clr, string tag)
         {
             Color = clr;
@@ -68,30 +74,39 @@ namespace LightBlueFox.Util.Logging
             Color = new Color(0, 0, 255, 255);
             Printable = false;
         }
+    #endregion
+
+        #region Log Levels
 
         /// <summary>
         /// Should be the default log level. Use this when normal actions are performed.
         /// </summary>
-        public static readonly LogLevel INFO = new LogLevel(new Color("#0efb44")   ,"INFOS");
+        public static readonly LogLevel INFO = new LogLevel(new Color("#0efb44")   ," INFOS ");
         /// <summary>
         /// Used in order to debug the program. All small details that are not important enough to be logged with <see cref="INFO"/> should be here.
         /// </summary>
-        public static readonly LogLevel DEBUG = new LogLevel(new Color("#bababa")  ,"DEBUG");
+        public static readonly LogLevel DEBUG = new LogLevel(new Color("#bababa")  ," DEBUG ");
         /// <summary>
         /// For when something occurs that might be dangerous in the future.
         /// </summary>
-        public static readonly LogLevel WARNING = new LogLevel(new Color("#fac70c"),"WARNG");
+        public static readonly LogLevel WARNING = new LogLevel(new Color("#fac70c"),"WARNING");
         /// <summary>
         /// Used whenever there is an error, but the program can still continue.
         /// </summary>
-        public static readonly LogLevel ERROR = new LogLevel(new Color("#fa350c")  ,"ERROR");
+        public static readonly LogLevel ERROR = new LogLevel(new Color("#fa350c")  ," ERROR ");
         /// <summary>
         /// Used when there is an error so severe that the program cannot continue normally.
         /// </summary>
-        public static readonly LogLevel FATAL = new LogLevel(new Color("#aa0000")  ,"FATAL");
+        public static readonly LogLevel FATAL = new LogLevel(new Color("#aa0000")  ," FATAL ");
+        /// <summary>
+        /// Used when an operation finished successfully
+        /// </summary>
+        public static readonly LogLevel SUCCESS = new LogLevel(new Color("#fc0324"), "SUCCESS");
         /// <summary>
         /// Combines all log levels.
         /// </summary>
         public static readonly LogLevel ALL = new LogLevel(FATAL, ERROR, WARNING, DEBUG, INFO);
+
+        #endregion
     }
 }
